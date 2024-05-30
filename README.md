0bff8c4555e2da8b48aed7dd815f6c5c226b6d07
Clerison <cosclerison@gmail.com>
2024-05-30 19:00:30 -0300

    :hammer: 02 - Classes, Atributos e Métodos

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Curso PHP POO (Programação orientado a Objeto)
+
+Iniciar o Servidor PHP CLI
+$ php -S 127.0.0.1:8000
--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
-echo "Inicio do Curso";
-phpinfo();
+
+class Pessoa {
+    public $idade;
+    public $name;
+    
+    public function Falar($name, $idade)
+    {
+        // $name = 'Clerison Oliveira';
+        // $idade = 40;
+        echo "Nome: " . $name . "</br> Idade: " . $idade;
+    }  
+    
+}
+
+$clerison = new Pessoa();
+
+$clerison->name = "Clerison Oliveira Da Silva";
+$clerison->idade = 40;
+$clerison->Falar("Clerison","40");
+
+
+// echo $clerison->name;
+
+// var_dump($clerison);
